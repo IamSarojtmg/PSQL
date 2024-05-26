1ed74595110106ba9738a4f4db07ca89242a6729
Saroj Tamang <sarojtmg992@gmail.com>
2024-05-26 10:58:46 +0100

    installed nodemon

--- server.js
+++ server.js
@@ -1,4 +1,6 @@
 const express = require('express')
+const studentRoutes = require('./src/student/routes')
+
 
 const app = express()
 const port = 3000
@@ -6,6 +8,8 @@ const port = 3000
 app.get('/',(req, res)=>{
 res.send('Hello kitty')
 })
+
+app.use('/api/v1/students', studentRoutes)
 
 app.listen(port, ()=>{
     console.log(`listening on ${port}`);
---
+++ src/student/controller.js
@@ -0,0 +1,7 @@
+const getStudents = (req,res)=>{
+    console.log('gettting students');
+}
+
+module.exports = {
+    getStudents
+}
