5b33bf89267f8b590da363dcc04d275b5950fedc
Saroj Tamang <sarojtmg992@gmail.com>
2024-05-29 11:38:16 +0100

    creating seeding

--- listen.js
+++ listen.js
@@ -1,7 +1,3 @@
-// require('dotenv').config();
-
-
-// const app = require('./server.js');
-// const { PORT = 9090 } = process.env;
-
-// app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
+const app = require('./server.js');
+const { PORT = 9090 } = process.env;
+app.listen(PORT, () => console.log(`Listening on ${PORT}...`));
--- server.js
+++ server.js
@@ -1,9 +1,10 @@
 const express = require('express')
 const studentRoutes = require('./src/student/routes')
+require('dotenv').config({ path: '.env.production' }); // Load environment variables from.env.production
 
 
 const app = express()
-const port = 3000
+// const port = 3000
 
 app.use(express.json())
 
@@ -13,8 +14,8 @@ res.send('Welcome to my APi')
 
 app.use('/api/v1/students', studentRoutes)
 
-app.listen(port, ()=>{
-    console.log(`listening on ${port}`);
-})
+// app.listen(port, ()=>{
+//     console.log(`listening on ${port}`);
+// })
 
 module.exports = app
